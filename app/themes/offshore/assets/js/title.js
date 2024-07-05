ec65ff93246c0abc1848e40679c48511ca6aacbe
antalikavasilkova <antalikavasilkova@gmail.com>
2024-07-05 13:36:10 +0300

    fix accounting

--- app/themes/offshore/assets/js/title.js
+++ app/themes/offshore/assets/js/title.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
 	function makeParalaxEffect(obName) {
 		$(document).scroll(function() {
 			var menuHeight = 100;
@@ -13,13 +12,13 @@ $(document).ready(function() {
 			var obOffset = $th.offset();
 			if(bot > 0) $("#page.title").removeClass("big");
 				else $("#page.title").addClass("big");
-// 			if(bot > obOffset.top-menuHeight) 
+// 			if(bot > obOffset.top-menuHeight)
 // 				$th.find(".fixedCont").addClass("fixed");
-// 			if(bot < obOffset.top-menuHeight) 
+// 			if(bot < obOffset.top-menuHeight)
 // 				$th.find(".fixedCont").removeClass("fixed");
-			if(bot > 2000) 
+			if(bot > 2000)
  				$("#sec1 .fixedCont").addClass("transparent");
- 			else if(bot < 2000) 
+ 			else if(bot < 2000)
  				$("#sec1 .fixedCont").removeClass("transparent");
 			if(bot > 400) {
 				$("#page.title header .bottom").addClass("showen");
@@ -28,12 +27,12 @@ $(document).ready(function() {
 			}
 		});
 	}
-	
+
 	//var is_safari = (navigator.userAgent.indexOf('Safari') != -1) && (navigator.userAgent.indexOf('Chrome') == -1);
 	//if (navigator.userAgent.match(/(iPod|iPhone|iPad)/) || is_safari) {
-	
+
 	makeParalaxEffect(".title-page-background #sec1");
-	
+
 	//}
 	//makeParalaxEffect(".title-page-background #sec2");
 	//makeParalaxEffect(".title-page-background #sec3");
